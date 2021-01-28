d229f69be0254888a3eecc82864acdd725feff2d
verena-km <30501721+verena-km@users.noreply.github.com>
2021-01-28 22:39:09 +0100

    Beispielcode hinzugefügt

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    vehicle.turn_left(100)
+    basic.pause(1000)
+    vehicle.drive(-80, -50)
+    basic.pause(1000)
+    vehicle.stop()
 })
+let vehicle: l298.Vehicle = null
+let motor = l298.create_motor(DigitalPin.P0, DigitalPin.P1, AnalogPin.C16)
+let motor2 = l298.create_motor(DigitalPin.P2, DigitalPin.P3, AnalogPin.C17)
+vehicle = l298.create_vehicle(motor2, motor)
